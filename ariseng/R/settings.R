532119d70755b429e74a36b3375ced9c3657f09b
Victor Ordu <victorordu@outlook.com>
2019-08-22 19:45:02 +0100

    Fixed bug
    There was a problem handling edge cases where the file already exists in the given directory. Optional overwriting (with prompting).

--- ariseng/R/settings.R
+++ ariseng/R/settings.R
@@ -8,7 +8,31 @@
 #' @export
 fetch_settings <- function(path = '.')
 {
+  path <- normalizePath(path)
   stopifnot(dir.exists(path))
-  f <- system.file('extdata', 'arise.dte', package = 'ariseng', mustWork = TRUE)
-  file.copy(f, file.path(path, basename(f)))
+  orig <-
+    system.file('extdata',
+                'arise.dte',
+                package = 'ariseng',
+                mustWork = TRUE)
+  cpy <- file.path(path, basename(orig))
+  
+  overwrite <- FALSE
+  if (file.exists(cpy)) {
+    opt <-
+      menu(
+        c("Yes", "No"),
+        graphics = TRUE,
+        title = sprintf(
+          "Overwrite %s?",
+          sQuote(basename(cpy))
+        )
+      )
+    if (opt == 2L)
+      return(FALSE)
+    else if (opt == 1L)
+      overwrite <- TRUE
+  }
+  
+  file.copy(orig, cpy, overwrite = overwrite)
 }
